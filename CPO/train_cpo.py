ed6b21c5a8adebc42fe44dddf71461ae3d56ae79
MasonN808 <mason.naka@gmail.com>
2023-07-21 17:46:13 -0700

    edits

--- CPO/train_cpo.py
+++ CPO/train_cpo.py
@@ -64,7 +64,7 @@ TASK_TO_CFG = {
 }
 import os
 os.environ["WANDB_API_KEY"] = '9762ecfe45a25eda27bb421e664afe503bb42297'
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1" 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0" 
 
 # Make my own config params
 @dataclass
