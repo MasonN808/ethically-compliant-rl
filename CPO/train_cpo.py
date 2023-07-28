e241e64857b395eba0cfb62432de8f09e87367b8
MasonN808 <mason.naka@gmail.com>
2023-07-28 10:22:26 -0700

    config edits

--- CPO/train_cpo.py
+++ CPO/train_cpo.py
@@ -87,18 +87,18 @@ class MyCfg(TrainCfg):
     # task: str = "SafetyPointCircle1Gymnasium-v0"
     task: str = "parking-v0"
     epoch: int = 100
-    lr: float = 0.001
+    lr: float = 0.01
     render: float = None # The rate at which it renders (e.g., .001)
     render_mode: str = None # "rgb_array" or "human" or None
     thread: int = 320 # If use CPU to train
-    step_per_epoch: int = 50000
+    step_per_epoch: int = 20000
     project: str = "fast-safe-rl"
     # slurm: bool = True
     worker: str = "ShmemVectorEnv"
     # worker: str = "RayVectorEnv"
     # Decide which device to use based on availability
     device: str = ("cuda" if torch.cuda.is_available() else "cpu")
-    gamma: float = .98
+    gamma: float = .99
     env_config_file: str = 'configs/ParkingEnv/env-image.txt'
 
 with open(MyCfg.env_config_file) as f:
