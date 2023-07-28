d3b97c9bb173cf621b9850496abc0bdbf686432e
MasonN808 <mason.naka@gmail.com>
2023-07-28 12:46:35 -0700

    edits

--- job.sh
+++ job.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 #SBATCH --job-name=safe-rl
-#SBATCH --cpus-per-task=4
 #SBATCH --mem=80gb
 #SBATCH --gpus=1
 #SBATCH --time=20:00:00
 
-srun  --mem=60gb --qos=medium --gpus=1 -c 10 --time=20:00:00 "/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_1.py" &
-srun  --mem=60gb --qos=medium --gpus=1 -c 10 --time=20:00:00 "/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_2.py" &
+srun "/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_1.py" &
+srun "/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_2.py" &
 wait
