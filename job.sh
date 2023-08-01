42379884aff3f0316cfb7fe062c87985f10274df
MasonN808 <mason.naka@gmail.com>
2023-08-01 10:16:03 -0700

    edits to job

--- job.sh
+++ job.sh
@@ -9,6 +9,6 @@
 #SBATCH --qos medium
 
 
-srun -n1 -N1 --gpus=1 --mem=50gb "/nas/ucb/mason/ethically-compliant-rl/SAC/train_sac.py"
+srun -n1 -N1 --gpus=1 --mem=50gb "/nas/ucb/mason/ethically-compliant-rl/SAC/train_SAC.py"
 # srun -n1 -N1 --gpus=1 --mem=50gb "/nas/ucb/mason/ethically-compliant-rl/PPO/train_ppo.py"
 # wait
