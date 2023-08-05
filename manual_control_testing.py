9fc81547bbf41096bccfa788a89c3582b21c7643
MasonN808 <mason.naka@gmail.com>
2023-08-04 17:46:17 -0700

    created a manual control file for testing

---
+++ manual_control_testing.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+import highway_env
+
+env = gym.make("parking-v0", render_mode="human")
+env.configure({
+    "observation": {
+        "type": "KinematicsGoal",
+        "features": ["x", "y", "vx", "vy", "cos_h", "sin_h"],
+        "scales": [100, 100, 5, 5, 1, 1], 
+        "normalize": False
+    },
+    "manual_control": True,
+    "real_time_rendering": True,
+    'cost_delta_distance': 2,
+    'add_walls': False,
+})
+env.reset()
+done = False
+while not done:
+    _, _, done, _, _ = env.step(env.action_space.sample())  # with manual control, these actions are ignored
+    env.render()
