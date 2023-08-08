44513dfce6f0450aee8e325c545058223858ba3d
MasonN808 <mason.naka@gmail.com>
2023-08-08 11:15:30 -0700

    edits to job for slurm run

--- job-3.sh
+++ job-3.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-#SBATCH --gpus-per-task=A6000:1
 #SBATCH --job-name=safe-rl
-#SBATCH --mem=40gb
-#SBATCH --time=20:00:00
-#SBATCH -c 4
-#SBATCH --gpus=1
-#SBATCH --qos=high
+#SBATCH --gpus-per-task=A6000:1
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=90gb
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=30:00:00
+#SBATCH --qos medium
 
 
-
+srun "/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py"
