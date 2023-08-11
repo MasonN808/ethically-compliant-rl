6811a7299f89122e3437c41bf6273327dd5c9b7a
MasonN808 <mason.naka@gmail.com>
2023-08-11 10:54:17 -0700

    edit job

--- job-2.sh
+++ job-2.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 #SBATCH --job-name=safe-rl
 #SBATCH --cpus-per-task=9
-#SBATCH --mem=50gb
+#SBATCH --mem=40gb
 #SBATCH --nodes=2
 #SBATCH --ntasks=2
 #SBATCH --time=30:00:00
-#SBATCH --qos default
+#SBATCH --qos scavenger
 
+SCRIPTS=(
+"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py"
+"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom_2.py"
+)
 
-srun -N1 -n1 "/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" &
-srun -N1 -n1 "/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom_2.py" &
+for SCRIPT in \${SCRIPTS[@]}; do
+    srun -N1 -n1 \$SCRIPT &
+done
 wait
