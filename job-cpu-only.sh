b635fa85a2fdb2211321b5c83f1b6ad18864b8d2
MasonN808 <mason.naka@gmail.com>
2023-08-11 15:25:23 -0700

    edits to jobs

---
+++ job-cpu-only.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=safe-rl
+#SBATCH --cpus-per-task=9
+#SBATCH --mem=30gb
+#SBATCH --nodes=3
+#SBATCH --ntasks=3
+#SBATCH --time=30:00:00
+#SBATCH --qos scavenger
+
+SCRIPTS=(
+"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" 
+"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" --cost_limit 10.0 10.0
+"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" --target_kl 0.015
+)
+
+for SCRIPT in ${SCRIPTS[@]}; do
+    srun -N1 -n1 python3 $SCRIPT &
+done
+wait
