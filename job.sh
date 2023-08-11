6b685ec7b2d79732916f8236d17449a9ee1ee171
MasonN808 <mason.naka@gmail.com>
2023-08-11 15:20:42 -0700

    edits to job

--- job.sh
+++ job.sh
@@ -2,17 +2,17 @@
 #SBATCH --job-name=safe-rl
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=5
-#SBATCH --mem=60gb
-#SBATCH --nodes=2
-#SBATCH --ntasks=2
+#SBATCH --mem=30gb
+#SBATCH --nodes=3
+#SBATCH --ntasks=3
 #SBATCH --time=30:00:00
 #SBATCH --qos scavenger
 
 
 SCRIPTS=(
 "/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" 
-"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" 
-"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" 
+"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" --cost_limit 10.0 10.0
+"/nas/ucb/mason/ethically-compliant-rl/CPO/train_cpo_custom.py" --target_kl 0.015
 )
 
 for SCRIPT in ${SCRIPTS[@]}; do
