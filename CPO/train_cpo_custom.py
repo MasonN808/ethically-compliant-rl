18fa05b05675fd82f8e68eca1995895ab2dd9909
MasonN808 <mason.naka@gmail.com>
2023-08-14 21:22:24 -0700

    edits to job

--- CPO/train_cpo_custom.py
+++ CPO/train_cpo_custom.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append("FSRL")
 from fsrl.utils.net.common import ActorCritic
 # Set this before everything
-os. environ['WANDB_DISABLED'] = 'True'
+os. environ['WANDB_DISABLED'] = 'False'
 os.environ["WANDB_API_KEY"] = '9762ecfe45a25eda27bb421e664afe503bb42297'
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
