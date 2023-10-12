2e187eacb17c787b3287ac8589a415ac9139258f
MasonN808 <mason.naka@gmail.com>
2023-10-11 20:49:30 -0400

    testing cpo with one constraint with set seed functions

--- CPO/train_cpo.py
+++ CPO/train_cpo.py
@@ -3,7 +3,7 @@ import os
 import wandb
 
 # wandb.init(entity="mason-nakamura1", project="CPO-sweep-NoWalls")
-os. environ['WANDB_DISABLED'] = 'True'
+# os. environ['WANDB_DISABLED'] = 'True'
 # os.environ["WANDB_API_KEY"] = '9762ecfe45a25eda27bb421e664afe503bb42297'
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1" # For GPU identification
