9cb4ef189f2f430f2eea028cc6b12dc29c6cc5aa
MasonN808 <mason.naka@gmail.com>
2023-10-11 21:02:48 -0400

    edits to PPO and enviornment

--- PPO/train_ppo.py
+++ PPO/train_ppo.py
@@ -1,7 +1,22 @@
+import ast
 import gym
+import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.envs import DummyVecEnv
 from stable_baselines3.common.policies import MlpPolicy
+from PPO.utils import load_environment
+
+with open('configs/ParkingEnv/env-default.txt') as f:
+    data = f.read()
+# Reconstructing the data as a dictionary
+ENV_CONFIG = ast.literal_eval(data)
+# Overriding certain keys in the environment config
+ENV_CONFIG.update({
+    "start_angle": -np.math.pi/2, # This is radians
+})
+
+# load the Highway env from the config file
+demo_env = load_environment(ENV_CONFIG)
 
 # Create the environment
 env = gym.make('CartPole-v1')
---
+++ PPO/utils.py
@@ -0,0 +1,42 @@
+import json
+import gymnasium as gym
+import logging
+import numpy as np
+logger = logging.getLogger(__name__)
+
+def load_environment(env_config, render_mode=None):
+    """
+        Load an environment from a configuration file.
+
+    :param env_config: the configuration, or path to the environment configuration file
+    :return: the environment
+    """
+    # Load the environment config from file
+    if not isinstance(env_config, dict):
+        with open(env_config) as f:
+            env_config = json.loads(f.read())
+
+    # Make the environment
+    if env_config.get("import_module", None):
+        __import__(env_config["import_module"])
+    try:
+        env = gym.make(env_config['id'], render_mode=render_mode)
+        # Save env module in order to be able to import it again
+        env.import_module = env_config.get("import_module", None)
+    except KeyError:
+        raise ValueError("The gym register-id of the environment must be provided")
+    except gym.error.UnregisteredEnv:
+        # The environment is unregistered.
+        print("import_module", env_config["import_module"])
+        raise gym.error.UnregisteredEnv('Environment {} not registered. The environment module should be specified by '
+                                        'the "import_module" key of the environment configuration'.format(
+                                            env_config['id']))
+
+    # Configure the environment, if supported
+    try:
+        env.unwrapped.configure(env_config)
+        # Reset the environment to ensure configuration is applied
+        env.reset()
+    except AttributeError as e:
+        logger.info("This environment does not support configuration. {}".format(e))
+    return env
