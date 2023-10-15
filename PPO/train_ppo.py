72caa44eecba78037599d8708ee3ed0c5967ea24
MasonN808 <mason.naka@gmail.com>
2023-10-15 13:23:17 -0400

    refactored imports

--- PPO/train_ppo.py
+++ PPO/train_ppo.py
@@ -1,14 +1,11 @@
 import ast
-from typing import Any, Dict
 import numpy as np
 import torch
 import wandb
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.ppo import MlpPolicy
-from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.logger import Logger
 from utils import load_environment
 from gymnasium.wrappers import FlattenObservation
 
