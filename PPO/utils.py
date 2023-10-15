60d47b0d2255c6aa5988b6f70c8a20a97fcce269
MasonN808 <mason.naka@gmail.com>
2023-10-15 11:58:38 -0400

    added evaluator for PPO to generate gifs

--- PPO/utils.py
+++ PPO/utils.py
@@ -1,4 +1,5 @@
 import json
+import imageio
 import gymnasium as gym
 import logging
 import numpy as np
@@ -40,3 +41,34 @@ def load_environment(env_config, render_mode=None):
     except AttributeError as e:
         logger.info("This environment does not support configuration. {}".format(e))
     return env
+
+
+# Replaces evaluate_policy() from stable_baselines3 with a version that outputs frames to be transformed into a gif
+def evaluate_policy_and_capture_frames(model, env, n_eval_episodes=10):
+    episode_rewards = []
+    frames = []
+
+    for _ in range(n_eval_episodes):
+        obs = env.reset()
+        done = False
+        episode_reward = 0
+
+        while not done:
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, done, _ = env.step(action)
+            episode_reward += reward
+            
+            # Capture frame
+            frame = env.render(mode='rgb_array')
+            frames.append(frame)
+
+        episode_rewards.append(episode_reward)
+    
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+    
+    return mean_reward, std_reward, frames
+
+# Duration is same as fpss
+def save_frames_as_gif(frames, path='./gym_animation.gif', duration=30):
+    imageio.mimsave(path, frames, duration=duration)
