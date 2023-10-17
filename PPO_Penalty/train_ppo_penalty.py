c121c6f42c05a1a73cf2ccbbdd4f411e2c73e1b0
MasonN808 <mason.naka@gmail.com>
2023-10-17 14:22:04 -0400

    this code doesn't work: will migrate to PPO from stable baselines and edit the source

--- PPO_Penalty/train_ppo_penalty.py
+++ PPO_Penalty/train_ppo_penalty.py
@@ -7,10 +7,6 @@ Based on:
 2. Proximal Policy Optimization Algorithms (OpenAI): [https://arxiv.org/abs/1707.06347]
 
 View more on my tutorial website: https://morvanzhou.github.io/tutorials
-
-Dependencies:
-tensorflow r1.2
-gym 0.9.2
 """
 
 import ast
@@ -23,6 +19,7 @@ from gymnasium.wrappers import FlattenObservation
 from stable_baselines3.common.vec_env import DummyVecEnv
 from utils import load_environment
 import wandb
+import tensorflow_probability as tfp
 
 EP_MAX = 1000
 EP_LEN = 200
@@ -63,130 +60,123 @@ ENV_CONFIG.update({
 class PPO(object):
 
     def __init__(self):
-        self.sess = tf.Session()
-        self.tfs = tf.placeholder(tf.float32, [None, S_DIM], 'state')
+        self.tfs = tf.constant(np.zeros((1, S_DIM), dtype=np.float32), dtype=tf.float32)
 
         # critic
-        with tf.variable_scope('critic'):
-            l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu)
-            self.v = tf.layers.dense(l1, 1)
-            self.tfdc_r = tf.placeholder(tf.float32, [None, 1], 'discounted_r')
-            self.advantage = self.tfdc_r - self.v
-            self.closs = tf.reduce_mean(tf.square(self.advantage))
-            self.ctrain_op = tf.train.AdamOptimizer(C_LR).minimize(self.closs)
+        self.critic = self.build_critic()
+        self.critic_optimizer = tf.keras.optimizers.Adam(learning_rate=C_LR)
 
         # actor
-        pi, pi_params = self._build_anet('pi', trainable=True)
-        oldpi, oldpi_params = self._build_anet('oldpi', trainable=False)
-        with tf.variable_scope('sample_action'):
-            self.sample_op = tf.squeeze(pi.sample(1), axis=0)       # choosing action
-        with tf.variable_scope('update_oldpi'):
-            self.update_oldpi_op = [oldp.assign(p) for p, oldp in zip(pi_params, oldpi_params)]
+        self.actor = self.build_actor()
+        self.actor_optimizer = tf.keras.optimizers.Adam(learning_rate=A_LR)
 
-        self.tfa = tf.placeholder(tf.float32, [None, A_DIM], 'action')
-        self.tfadv = tf.placeholder(tf.float32, [None, 1], 'advantage')
-        with tf.variable_scope('loss'):
-            with tf.variable_scope('surrogate'):
-                # ratio = tf.exp(pi.log_prob(self.tfa) - oldpi.log_prob(self.tfa))
-                ratio = pi.prob(self.tfa) / (oldpi.prob(self.tfa) + 1e-5)
-                surr = ratio * self.tfadv
-            if METHOD['name'] == 'kl_pen':
-                self.tflam = tf.placeholder(tf.float32, None, 'lambda')
-                kl = tf.distributions.kl_divergence(oldpi, pi)
-                self.kl_mean = tf.reduce_mean(kl)
-                self.aloss = -(tf.reduce_mean(surr - self.tflam * kl))
-            else:   # clipping method, find this is better
-                self.aloss = -tf.reduce_mean(tf.minimum(
-                    surr,
-                    tf.clip_by_value(ratio, 1.-METHOD['epsilon'], 1.+METHOD['epsilon'])*self.tfadv))
+    def build_critic(self):
+        critic = tf.keras.Sequential([
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(1)
+        ])
+        return critic
 
-        with tf.variable_scope('atrain'):
-            self.atrain_op = tf.train.AdamOptimizer(A_LR).minimize(self.aloss)
+    def build_actor(self):
+        inputs = tf.keras.layers.Input(shape=(S_DIM,))
+        x = tf.keras.layers.Dense(100, activation='relu')(inputs)
+        mu = 2 * tf.keras.layers.Dense(A_DIM, activation='tanh')(x)
+        sigma = tf.keras.layers.Dense(A_DIM, activation='softplus')(x)
+        actor = tf.keras.Model(inputs, [mu, sigma])
+        return actor
 
-        tf.summary.FileWriter("log/", self.sess.graph)
+    def ppo_loss(self, y_true, y_pred):
+        old_mu, old_sigma = y_true[:, :A_DIM], y_true[:, A_DIM:]
+        mu, sigma = y_pred[:, :A_DIM], y_pred[:, A_DIM:]
+        td_error = tf.reduce_mean(self.advantage() * self.old_log_prob() / (old_sigma + 1e-5))
+        return -td_error
 
-        self.sess.run(tf.global_variables_initializer())
+    def advantage(self):
+        return self.tfdc_r - self.critic(self.tfs)
 
-    def update(self, s, a, r):
-        self.sess.run(self.update_oldpi_op)
-        adv = self.sess.run(self.advantage, {self.tfs: s, self.tfdc_r: r})
-        # adv = (adv - adv.mean())/(adv.std()+1e-6)     # sometimes helpful
+    def old_log_prob(self):
+        old_mu, old_sigma = self.oldpi()
+        old_dist = tfp.distributions.Normal(loc=old_mu, scale=old_sigma)
+        return old_dist.log_prob(self.tfa)
 
-        # update actor
-        if METHOD['name'] == 'kl_pen':
-            for _ in range(A_UPDATE_STEPS):
-                _, kl = self.sess.run(
-                    [self.atrain_op, self.kl_mean],
-                    {self.tfs: s, self.tfa: a, self.tfadv: adv, self.tflam: METHOD['lam']})
-                if kl > 4*METHOD['kl_target']:  # this in in google's paper
-                    break
-            if kl < METHOD['kl_target'] / 1.5:  # adaptive lambda, this is in OpenAI's paper
-                METHOD['lam'] /= 2
-            elif kl > METHOD['kl_target'] * 1.5:
-                METHOD['lam'] *= 2
-            METHOD['lam'] = np.clip(METHOD['lam'], 1e-4, 10)    # sometimes explode, this clipping is my solution
-        else:   # clipping method, find this is better (OpenAI's paper)
-            [self.sess.run(self.atrain_op, {self.tfs: s, self.tfa: a, self.tfadv: adv}) for _ in range(A_UPDATE_STEPS)]
-
-        # update critic
-        [self.sess.run(self.ctrain_op, {self.tfs: s, self.tfdc_r: r}) for _ in range(C_UPDATE_STEPS)]
-
-    def _build_anet(self, name, trainable):
-        with tf.variable_scope(name):
-            l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu, trainable=trainable)
-            mu = 2 * tf.layers.dense(l1, A_DIM, tf.nn.tanh, trainable=trainable)
-            sigma = tf.layers.dense(l1, A_DIM, tf.nn.softplus, trainable=trainable)
-            norm_dist = tf.distributions.Normal(loc=mu, scale=sigma)
-        params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
-        return norm_dist, params
+    def oldpi(self):
+        mu, sigma = self.actor(self.tfs)
+        old_mu, old_sigma = tf.stop_gradient(mu), tf.stop_gradient(sigma)
+        return old_mu, old_sigma
 
     def choose_action(self, s):
         s = s[np.newaxis, :]
-        a = self.sess.run(self.sample_op, {self.tfs: s})[0]
-        return np.clip(a, -2, 2)
+        mu, sigma = self.actor(s)
+        a = np.clip(np.random.normal(mu, sigma), -2, 2)
+        return a
 
     def get_v(self, s):
         if s.ndim < 2: s = s[np.newaxis, :]
-        return self.sess.run(self.v, {self.tfs: s})[0, 0]
+        return self.critic(s)[0, 0]
 
-env = FlattenObservation(load_environment(ENV_CONFIG))
-env = DummyVecEnv([lambda: env])
-ppo = PPO()
-all_ep_r = []
-
-for ep in range(EP_MAX):
-    s = env.reset()
-    buffer_s, buffer_a, buffer_r = [], [], []
-    ep_r = 0
-    for t in range(EP_LEN):    # in one episode
-        env.render()
-        a = ppo.choose_action(s)
-        s_, r, done, _ = env.step(a)
-        buffer_s.append(s)
-        buffer_a.append(a)
-        buffer_r.append((r+8)/8)    # normalize reward, find to be useful
-        s = s_
-        ep_r += r
-
-        # update ppo
-        if (t+1) % BATCH == 0 or t == EP_LEN-1:
-            v_s_ = ppo.get_v(s_)
+    def update(self, s, a, r):
+        with tf.GradientTape() as tape:
+            v_s_ = self.get_v(s_)
             discounted_r = []
             for r in buffer_r[::-1]:
                 v_s_ = r + GAMMA * v_s_
                 discounted_r.append(v_s_)
             discounted_r.reverse()
-
             bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
-            buffer_s, buffer_a, buffer_r = [], [], []
-            ppo.update(bs, ba, br)
-    if ep == 0: all_ep_r.append(ep_r)
-    else: all_ep_r.append(all_ep_r[-1]*0.9 + ep_r*0.1)
-    print(
-        'Ep: %i' % ep,
-        "|Ep_r: %i" % ep_r,
-        ("|Lam: %.4f" % METHOD['lam']) if METHOD['name'] == 'kl_pen' else '',
-    )
 
-plt.plot(np.arange(len(all_ep_r)), all_ep_r)
-plt.xlabel('Episode');plt.ylabel('Moving averaged episode reward');plt.show()
+            # actor loss
+            mu, sigma = self.actor(bs)
+            old_mu, old_sigma = self.oldpi()
+            ratio = tf.exp(self.actor.log_prob(ba) - self.old_log_prob())
+            surr = ratio * self.advantage()
+            if METHOD['name'] == 'kl_pen':
+                kl = tfp.distributions.kl_divergence(
+                    tfp.distributions.Normal(loc=old_mu, scale=old_sigma),
+                    tfp.distributions.Normal(loc=mu, scale=sigma)
+                )
+                kl_mean = tf.reduce_mean(kl)
+                aloss = -(tf.reduce_mean(surr - METHOD['lam'] * kl))
+            else:
+                aloss = -tf.reduce_mean(tf.minimum(surr, tf.clip_by_value(ratio, 1. - METHOD['epsilon'], 1. + METHOD['epsilon']) * self.advantage()))
+
+            # critic loss
+            closs = tf.reduce_mean(tf.square(self.advantage()))
+
+            total_loss = aloss + closs
+            gradients = tape.gradient(total_loss, self.actor.trainable_variables + self.critic.trainable_variables)
+            self.actor_optimizer.apply_gradients(zip(gradients, self.actor.trainable_variables + self.critic.trainable_variables))
+
+if __name__ == "__main__":
+    env = FlattenObservation(load_environment(ENV_CONFIG))
+    env = DummyVecEnv([lambda: env])
+    ppo = PPO()
+    all_ep_r = []
+
+    for ep in range(EP_MAX):
+        s = env.reset()
+        buffer_s, buffer_a, buffer_r = [], [], []
+        ep_r = 0
+        for t in range(EP_LEN):    # in one episode
+            env.render()
+            a = ppo.choose_action(s)
+            s_, r, done, _ = env.step(a)
+            buffer_s.append(s)
+            buffer_a.append(a)
+            buffer_r.append((r+8)/8)    # normalize reward, find to be useful
+            s = s_
+            ep_r += r
+
+            # update ppo
+            if (t+1) % BATCH == 0 or t == EP_LEN-1:
+                ppo.update(buffer_s, buffer_a, buffer_r)
+                buffer_s, buffer_a, buffer_r = [], [], []
+        if ep == 0: all_ep_r.append(ep_r)
+        else: all_ep_r.append(all_ep_r[-1]*0.9 + ep_r*0.1)
+        print(
+            'Ep: %i' % ep,
+            "|Ep_r: %i" % ep_r,
+            ("|Lam: %.4f" % METHOD['lam']) if METHOD['name'] == 'kl_pen' else '',
+        )
+
+    plt.plot(np.arange(len(all_ep_r)), all_ep_r)
+    plt.xlabel('Episode');plt.ylabel('Moving averaged episode reward');plt.show()
