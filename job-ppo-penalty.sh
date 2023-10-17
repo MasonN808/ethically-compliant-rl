57c9b439933cbcfed255f81ae45d51686a7b19cc
MasonN808 <mason.naka@gmail.com>
2023-10-17 18:58:50 -0400

    removed GPU since low utilization due to small batch size and small NN

--- job-ppo-penalty.sh
+++ job-ppo-penalty.sh
@@ -2,7 +2,6 @@
 
 # SLURM settings for the job submission
 #SBATCH --job-name=ppo-penalty         # Name of the job
-#SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=8         # Number of CPUs per task
 #SBATCH --mem=20gb                # Memory allocated
 #SBATCH --nodes=4                 # Number of nodes
