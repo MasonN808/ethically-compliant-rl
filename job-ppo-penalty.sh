1d83e938196713ea322bd55d4ff4177a7d39a15a
MasonN808 <mason.naka@gmail.com>
2023-10-17 23:42:25 -0400

    running another ppo run

--- job-ppo-penalty.sh
+++ job-ppo-penalty.sh
@@ -2,11 +2,11 @@
 
 # SLURM settings for the job submission
 #SBATCH --job-name=ppo-penalty         # Name of the job
-#SBATCH --cpus-per-task=8         # Number of CPUs per task
-#SBATCH --mem=20gb                # Memory allocated
+#SBATCH --cpus-per-task=5         # Number of CPUs per task
+#SBATCH --mem=45gb                # Memory allocated
 #SBATCH --nodes=4                 # Number of nodes
-#SBATCH --ntasks=4                # Number of tasks
-#SBATCH --time=20:00:00         # Maximum run time of the job (set to 3 days)
+#SBATCH --ntasks=8                # Number of tasks
+#SBATCH --time=24:00:00         # Maximum run time of the job (set to 3 days)
 #SBATCH --qos=scavenger           # Quality of Service of the job
 
 # Activate python environment, if you use one (e.g., conda or virtualenv)
@@ -21,7 +21,7 @@ SCRIPTS=(
     "$BASE_SCRIPT $ARGS" 
 )
 
-BETA_VALUES=("0.0" ".01" ".05" "40")  # Add the beta values you want to test
+BETA_VALUES=("0.0" ".01" "0.05" "0.1" "0.3" "1.0" "3" "10" "40")  # Add the beta values you want to test
 
 for SCRIPT in "${SCRIPTS[@]}"; do
     for BETA in "${BETA_VALUES[@]}"; do
