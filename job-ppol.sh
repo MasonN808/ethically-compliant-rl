f14090ba9f9b523391a354707701692924a344ee
MasonN808 <mason.naka@gmail.com>
2023-10-31 00:02:26 -0400

    edits to job

--- job-ppol.sh
+++ job-ppol.sh
@@ -18,10 +18,10 @@ ARGS="$@"
 BASE_SCRIPT="/nas/ucb/mason/ethically-compliant-rl/PPOL/train_ppol.py"
 
 # Extract number of nodes from SLURM settings
-NUM_NODES=$(grep "^#SBATCH --nodes=" $0 | cut -d'=' -f2)
+# NUM_NODES=$(grep "^#SBATCH --nodes=" $0 | cut -d'=' -f2)
 
 # Run the script as many times as the number of nodes in parallel
-for i in $(seq 1 $NUM_NODES); do
+for i in {1..4}; do
     srun -N1 -n1 python3 $BASE_SCRIPT $ARGS &
 done
 
