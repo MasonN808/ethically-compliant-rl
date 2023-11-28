27349b9ff3e502332fbf64f95c1050ffd3aecae3
MasonN808 <mason.naka@gmail.com>
2023-11-28 17:58:51 -0500

    minor edits to manual control testing

--- manual_control_testing.py
+++ manual_control_testing.py
@@ -18,7 +18,7 @@ env.configure({
     "show_trajectories": False,
     "success_goal_reward": 0.12,
     "collision_reward": -5,
-    "simulation_frequency": 90,
+    "simulation_frequency": 100,
     "policy_frequency": 30,
     "duration": 70,
     "screen_width": 600,
@@ -44,13 +44,13 @@ while not done:
     obs, rew, done, _, info = env.step(env.action_space.sample())  # with manual control, these actions are ignored
     achieved_goal = obs['achieved_goal'][5]
     desired_goal = obs['desired_goal'][5]
-    print(f'achieved_goal: {achieved_goal}')
-    print(f'desired_goal: {desired_goal}')
+    # print(f'achieved_goal: {achieved_goal}')
+    # print(f'desired_goal: {desired_goal}')
     # print(info)
     # cost = info['cost']
     # print(f'cost: {cost}')
     # print(f'reward: {rew}')
-    print(rew)
+    # print(rew)
     # print(len(obs))
 
     env.render()
