b015af8af8d41e3b0b6ca717326fbff1adeb5715
MasonN808 <mason.naka@gmail.com>
2023-12-02 13:01:55 -0500

    updeatd bash

--- job-ppo-penalty.sh
+++ job-ppo-penalty.sh
@@ -3,10 +3,10 @@
 # SLURM settings for the job submission
 #SBATCH --job-name=ppo-penalty         # Name of the job
 #SBATCH --cpus-per-task=5         # Number of CPUs per task
-#SBATCH --mem=15gb                # Memory allocated
-#SBATCH --nodes=1                 # Number of nodes
-#SBATCH --ntasks=1                # Number of tasks
-#SBATCH --time=1-00:00:00         # Maximum run time of the job (set to 3 days)
+#SBATCH --mem=10gb                # Memory allocated
+#SBATCH --nodes=4                 # Number of nodes
+#SBATCH --ntasks=8                # Number of tasks
+#SBATCH --time=3-00:00:00         # Maximum run time of the job (set to 3 days)
 #SBATCH --qos=scavenger           # Quality of Service of the job
 
 # Activate python environment, if you use one (e.g., conda or virtualenv)
