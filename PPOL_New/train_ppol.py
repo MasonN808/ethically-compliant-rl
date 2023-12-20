373c773e578139be7de12020b117ef38dc342651
MasonN808 <mason.naka@gmail.com>
2023-12-19 16:10:53 -1000

    added utils and config files

--- PPOL_New/train_ppol.py
+++ PPOL_New/train_ppol.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 import ast
+import os
+# Enables WandB cloud syncing
+os.environ['WANDB_DISABLED'] = 'True'
 import numpy as np
 import torch
 import wandb
 import sys
 sys.path.append("stable_baselines3")
-from stable_baselines3 import PPO
+from stable_baselines3 import PPOL
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3.common.callbacks import BaseCallback
 from utils import load_environment
 from gymnasium.wrappers import FlattenObservation
-from PPOL_New.ppol_cfg import TrainCfg
+from ppol_cfg import TrainCfg
+from dataclasses import dataclass
+import pyrallis
 
 
 class WandbLoggingCallback(BaseCallback):
@@ -28,12 +33,14 @@ class WandbLoggingCallback(BaseCallback):
         # Continue training
         return True
     
+@dataclass
 class Cfg(TrainCfg):
     wandb_project_name: str = "New-PPOL"
     env_config: str = "configs/ParkingEnv/env-default.txt"
     epochs: int = 400
     total_timesteps: int = 100000
 
+@pyrallis.wrap()
 def train(args: Cfg):
     wandb.init(name="ppol-highway-parking", project=args.wandb_project_name, sync_tensorboard=True)
 
@@ -55,7 +62,7 @@ def train(args: Cfg):
     callback = WandbLoggingCallback()
 
     # Initialize the PPO agent with an MLP policy
-    agent = PPO(MlpPolicy, env, verbose=1)
+    agent = PPOL(MlpPolicy, env, verbose=1)
 
     # Train the agent with the callback
     for i in range(args.epochs):
@@ -70,3 +77,6 @@ def train(args: Cfg):
         print(f"Final reward: {rewards.mean()}")
         env.render()
     env.close()
+
+if __name__ == "__main__":
+    train()
