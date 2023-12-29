22754911121b7c74d7eea14132de1827dd1ae9de
MasonN808 <mason.naka@gmail.com>
2023-12-28 23:14:44 -1000

    enabled wandb

--- PPOL_New/train_ppol_high_limit.py
+++ PPOL_New/train_ppol_high_limit.py
@@ -3,7 +3,7 @@ import argparse
 import ast
 import os
 # Enables WandB cloud syncing
-os.environ['WANDB_DISABLED'] = 'True'
+os.environ['WANDB_DISABLED'] = 'False'
 os.environ["WANDB_API_KEY"] = '9762ecfe45a25eda27bb421e664afe503bb42297'
 import numpy as np
 import wandb
