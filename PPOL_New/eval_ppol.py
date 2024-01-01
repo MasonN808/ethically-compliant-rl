504f7bf31ef3246a70e5be7d3edb4c43c90eac5b
MasonN808 <mason.naka@gmail.com>
2023-12-31 22:27:02 -1000

    evaluations are poor

--- PPOL_New/eval_ppol.py
+++ PPOL_New/eval_ppol.py
@@ -53,7 +53,7 @@ def evaluate(args: TrainCfg):
     env = DummyVecEnv([lambda: env])
 
     # Path to your saved model
-    path_to_zip_file = "PPOL_New/models/New-PPOL-SpeedLimit=8/model_epoch(20)_timesteps(100000).zip"
+    path_to_zip_file = "PPOL_New/models/New-PPOL-NoMultipiler-SpeedLimit=2/lxczzdw4/model_epoch(25).zip"
 
     # Load the saved data
     data, params, _ = load_from_zip_file(path_to_zip_file)
--- PPOL_New/ppol_cfg.py
+++ PPOL_New/ppol_cfg.py
@@ -4,21 +4,13 @@ from dataclasses import dataclass, field
 @dataclass
 class TrainCfg:
     # general task params
-    task: str = "SafetyCarCircle-v0"
-    device: str = "cpu"
+    batch_size: int = 512
+    num_envs: int = 1
+
     # Lagrangian Parameters
     constraint_type: list[str] = field(default_factory=lambda: ["speed"])
-    cost_threshold: list[float] = field(default_factory=lambda: [2])
-    # K_P: float = 1
-    # K_I: float = 1
-    # K_D: float = 2
-    K_P: float = 0.05
-    K_I: float = 0.0005
-    K_D: float = 0.1
-    # logger params
-    logdir: str = "logs"
-    project: str = "fast-safe-rl"
-    group: Optional[str] = None
-    name: Optional[str] = None
-    prefix: Optional[str] = "ppol"
-    suffix: Optional[str] = ""
+    cost_threshold: list[float] = field(default_factory=lambda: [8])
+    lagrange_multiplier: bool = False
+    K_P: float = 1
+    K_I: float = 1
+    K_D: float = 2
