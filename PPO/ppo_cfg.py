1973256685fc04126bc61a243f82a2e452fd3220
MasonN808 <mason.naka@gmail.com>
2024-01-02 18:12:25 -1000

    added ppo config

---
+++ PPO/ppo_cfg.py
@@ -0,0 +1,9 @@
+from typing import Optional, Tuple
+from dataclasses import dataclass, field
+
+@dataclass
+class TrainCfg:
+    # general task params
+    epochs: int = 150
+    total_timesteps: int = 100000
+    batch_size: int = 256
