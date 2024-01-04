1f914242dd1156bd28e2bcb1f3ddc2caa673ad51
MasonN808 <mason.naka@gmail.com>
2024-01-03 22:33:59 -1000

    evaluation for ppo is done

--- PPO/eval_ppo.py
+++ PPO/eval_ppo.py
@@ -1,56 +1,77 @@
 import ast
+import os
+import sys
 import gym
 import numpy as np
 import torch
+import pyrallis
 from utils import load_environment, evaluate_policy_and_capture_frames, save_frames_as_gif
+sys.path.append("stable_baselines3")
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.ppo import MlpPolicy
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.save_util import load_from_zip_file
 from gymnasium.wrappers import FlattenObservation
+from ppo_cfg import TrainCfg
 
-seed = 10
-
-# Set the numpy seed
-np.random.seed(seed)
-
-# Set the pytorch seed
-# Set the seed for CPU
-torch.manual_seed(seed)
-
-# If you're using CUDA:
-if torch.cuda.is_available():
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-
-with open('configs/ParkingEnv/env-default.txt') as f:
-    data = f.read()
-
-# Reconstructing the data as a dictionary
-ENV_CONFIG = ast.literal_eval(data)
-# Overriding certain keys in the environment config
-ENV_CONFIG.update({
-    "start_angle": -np.math.pi/2, # This is radians
-})
-
-# Load the Highway env from the config file
-env = FlattenObservation(load_environment(ENV_CONFIG, render_mode="rgb_array"))
-
-# Stable baselines usually works with vectorized environments, 
-# so even though CartPole is a single environment, we wrap it in a DummyVecEnv
-env = DummyVecEnv([lambda: env])
-
-# Load the trained agent
-agent = PPO.load("PPO/models/model_epoch(7).zip", env=env)
-
-# A modified version of evaluate_policy() from stable_baslelines3
-mean_reward, std_reward, frames= evaluate_policy_and_capture_frames(agent, env, n_eval_episodes=7)
-
-# Create the gif from the frames
-save_frames_as_gif(frames, path=f'PPO/gifs/car_evaluation.gif')
+@pyrallis.wrap()
+def evaluate(args: TrainCfg):
+    for i in range(0, 120, 10):
+        # Path to your saved model
+        model_path = f"PPO/models/model_epoch(50)_timesteps(100000).zip"
+        # Parsing path for gif path
+        parsed_gif_file = model_path.split("/models/")[-1][:-4]
+        
+        # Parse the directory
+        # Splitting the string by '/'
+        parts = model_path.split('/')
+        parsed_gif_dir = parts[2] + '/' + parts[3]
 
 
-print(mean_reward, std_reward)
+        gif_dir = f"PPO/gifs/{parsed_gif_dir}"
+        gif_path = f"PPO/gifs/{parsed_gif_file}.gif"
+        # Create the path if it does not exist
+        if not os.path.exists(gif_dir):
+            os.makedirs(gif_dir)
+
+
+        with open('configs/ParkingEnv/env-default.txt') as f:
+            data = f.read()
+
+        # Reconstructing the data as a dictionary
+        ENV_CONFIG = ast.literal_eval(data)
+        # Overriding certain keys in the environment config
+        ENV_CONFIG.update({
+            "start_angle": -np.math.pi/2, # This is radians
+            "duration": 60,
+        })
+
+        # Load the Highway env from the config file
+        env = FlattenObservation(load_environment(ENV_CONFIG, render_mode="rgb_array"))
+
+        # Stable baselines usually works with vectorized environments, 
+        # so even though CartPole is a single environment, we wrap it in a DummyVecEnv
+        env = DummyVecEnv([lambda: env])
+
+        # Load the saved data
+        data, params, _ = load_from_zip_file(model_path)
+
+        # Load the trained agent
+        agent = PPO(
+                    policy=data["policy_class"], 
+                    env=env, 
+                    device='auto',
+                )
+        
+        # Load the model state
+        agent.set_parameters(params)
+
+        # A modified version of evaluate_policy() from stable_baslelines3
+        mean_reward, std_reward, frames= evaluate_policy_and_capture_frames(agent, env, n_eval_episodes=1)
+
+        # Create the gif from the frames
+        save_frames_as_gif(frames, path=gif_path)
+
+        print(mean_reward, std_reward)
+
+if __name__=="__main__":
+    evaluate()
