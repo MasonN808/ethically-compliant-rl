6c0e4dad0883a613d7228f95d295bb6b30aa0ec7
MasonN808 <mason.naka@gmail.com>
2024-01-08 21:46:31 -1000

    evaulation of new model

--- PPOL_New/eval_ppol.py
+++ PPOL_New/eval_ppol.py
@@ -22,7 +22,7 @@ from ppol_cfg import TrainCfg
 def evaluate(args: TrainCfg):
     for i in range(0, 1000, 10):
         # Path to your saved model
-        model_path = f"PPOL_New/models/New-PPOL-NoMultipiler-SpeedLimit=2/1ugan1k1/model_epoch({i}).zip"
+        model_path = f"PPOL_New/models/New-PPOL-SpeedLimit=10000/o7nxelt8/model_epoch({i}).zip"
         # Parsing path for gif path
         parsed_gif_file = model_path.split("/models/")[-1][:-4]
         
@@ -39,7 +39,7 @@ def evaluate(args: TrainCfg):
             os.makedirs(gif_dir)
 
 
-        with open('configs/ParkingEnv/env-default.txt') as f:
+        with open('configs/ParkingEnv/default.txt') as f:
             data = f.read()
 
         # Reconstructing the data as a dictionary
