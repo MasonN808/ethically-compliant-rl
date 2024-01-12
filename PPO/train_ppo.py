d1391e2390f0185e324c812a1967fb15326c94fb
MasonN808 <mason.naka@gmail.com>
2024-01-11 15:40:22 -1000

    testing new path

--- PPO/train_ppo.py
+++ PPO/train_ppo.py
@@ -12,7 +12,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.utils import set_random_seed
-sys.path.append('/path/to/your/utils')
+sys.path.append('utils')
 from utils import load_environment
 from gymnasium.wrappers import FlattenObservation
 import pyrallis
