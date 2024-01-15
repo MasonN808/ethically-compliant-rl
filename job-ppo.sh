ed9bad1d608c199b310c9a2c7d17b0b7a44385fc
MasonN808 <mason.naka@gmail.com>
2024-01-14 19:31:26 -1000

    job format is now equivalent

--- job-ppo.sh
+++ job-ppo.sh
@@ -3,9 +3,9 @@
 # SLURM settings for the job submission
 #SBATCH --job-name=ppo-parking         # Name of the job
 #SBATCH --cpus-per-task=3         # Number of CPUs per task
-#SBATCH --ntasks=3  # Specify the number of CPU cores
 #SBATCH --mem=8gb                # Memory allocated
 #SBATCH --nodes=3                 # Number of nodes
+#SBATCH --ntasks=3              # Specify the number of CPU cores
 #SBATCH --time=3-00:00:00         # Maximum run time of the job (set to 3 days)
 #SBATCH --qos=scavenger           # Quality of Service of the job
 
@@ -17,14 +17,8 @@ ARGS="$@"
 
 BASE_SCRIPT="/nas/ucb/mason/ethically-compliant-rl/PPO/train_ppo.py"
 
-SCRIPTS=(
-    "$BASE_SCRIPT $ARGS"
-)
-
 for i in {1..1}; do
-    for SCRIPT in "${SCRIPTS[@]}"; do
-        srun -N1 -n1 python3 $SCRIPT &
-    done
+    srun -N1 -n1 python3 $BASE_SCRIPT $ARGS &
 done
 
 wait
