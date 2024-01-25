e03a0f7d97f38d87554eef682d3e294a8d4ca841
MasonN808 <mason.naka@gmail.com>
2024-01-24 23:46:18 -1000

    edit ppol job

--- job-ppol.sh
+++ job-ppol.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=ppol-vanilla           # Name of the job
 #SBATCH --cpus-per-task=3         # Number of CPUs per task
 #SBATCH --mem=10gb                # Memory allocated
-#SBATCH --nodes=1                 # Number of nodes
-#SBATCH --ntasks=1                # Number of tasks
+#SBATCH --nodes=3                 # Number of nodes
+#SBATCH --ntasks=3                # Number of tasks
 #SBATCH --time=3-00:00:00           # Maximum run time of the job (set to 3 days)
 #SBATCH --qos=scavenger           # Quality of Service of the job
 
@@ -18,7 +18,7 @@ ARGS="$@"
 BASE_SCRIPT="/nas/ucb/mason/ethically-compliant-rl/PPOL_New/train_ppol.py"
 
 # Run the script four times in parallel
-for i in {1..1}; do
+for i in {1..3}; do
     srun -N1 -n1 python3 $BASE_SCRIPT $ARGS &
 done
 
