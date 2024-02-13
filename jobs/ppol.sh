67b847b8c3060feb9be0a740a066144839a8fb6d
MasonN808 <mason.naka@gmail.com>
2024-02-13 16:07:56 -0500

    increased time to 2 days

--- jobs/ppol.sh
+++ jobs/ppol.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=10gb                # Memory allocated
 #SBATCH --nodes=1                 # Number of nodes
 #SBATCH --ntasks=1                # Number of tasks
-#SBATCH --time=1-00:00:00           # Maximum run time of the job (set to 3 days)
+#SBATCH --time=2-00:00:00           # Maximum run time of the job (set to 3 days)
 #SBATCH --qos=scavenger           # Quality of Service of the job
 
 # Activate python environment, if you use one (e.g., conda or virtualenv)
