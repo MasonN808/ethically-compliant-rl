7c6c843c8f90f8c799c0c98fb47967f3f4b0a9a1
MasonN808 <mason.naka@gmail.com>
2024-02-21 22:23:09 -0500

    tested additional features functionality

--- manual_control_testing.py
+++ manual_control_testing.py
@@ -32,6 +32,7 @@ env.configure({
     "start_location": [40, 30],
     "manual_control": True,
     "extra_lines": True,
+    "additional_features": True,
 
     # Costs
     "constraint_type":["lines"],
@@ -43,17 +44,17 @@ done = False
 i=0
 while not done:
     i+=1
-    print(i)
+    # print(i)
     obs, rew, done, _, info = env.step(env.action_space.sample())  # with manual control, these actions are ignored
     achieved_goal = obs['achieved_goal'][5]
     desired_goal = obs['desired_goal'][5]
     # print(f'achieved_goal: {achieved_goal}')
     # print(f'desired_goal: {desired_goal}')
     # print(info)
-    cost = info['cost']
-    print(f'cost: {cost}')
+    # cost = info['cost']
+    # print(f'cost: {cost}')
     # print(f'reward: {rew}')
     # print(rew)
-    # print(len(obs))
+    print(obs)
 
     env.render()
