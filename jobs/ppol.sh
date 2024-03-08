15cd042fea7cbc57fe97de265750736c91995ce8
MasonN808 <mason.naka@gmail.com>
2024-03-08 16:17:23 -0500

    ready to run lidar observations

--- jobs/ppol.sh
+++ jobs/ppol.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # SLURM settings for the job submission
-#SBATCH --job-name=ppol-lines-large-net           # Name of the job
-#SBATCH --cpus-per-task=6         # Number of CPUs per task
-#SBATCH --mem=7gb                # Memory allocated
+#SBATCH --job-name=ppol-lines-lidar          # Name of the job
+#SBATCH --cpus-per-task=3         # Number of CPUs per task
+#SBATCH --mem=3gb                # Memory allocated
 #SBATCH --nodes=4                 # Number of nodes
 #SBATCH --ntasks=4                # Number of tasks
 #SBATCH --time=3-00:00:00           # Maximum run time of the job (set to 3 days)
@@ -15,12 +15,12 @@ source .venv/bin/activate
 # Get all arguments passed to the script
 ARGS="$@"
 
-BASE_SCRIPT="/nas/ucb/mason/ethically-compliant-rl/tests/PPOL_New/train_ppol.py"
+BASE_SCRIPT="/nas/ucb/mason/ethically-compliant-rl/tests/PPOL_New/train_ppol_lidar.py"
 
 # srun -N1 -n1 python3 $BASE_SCRIPT $ARGS
 
 SEEDS=("1")  # Vary seeds
-ENT_COEFS=(".004" ".008" ".01" ".012")  # Vary entropy coefficents
+ENT_COEFS=(".001" ".002" ".004" ".008")  # Vary entropy coefficents
 
 # Run the script as many times as the number of nodes in parallel
 for SEED in "${SEEDS[@]}"; do
